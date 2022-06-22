7768ef568619d9641fa9032e7cb65505b903e50f
doctype-Melvin <doctype.melvin@gmail.com>
2022-06-22 11:21:28 +0200

    adds fn to add elements to local storage

--- src/app.js
+++ src/app.js
@@ -8,6 +8,48 @@ Storage.prototype.getObj = function (key) {
     return value && JSON.parse(value);
 }
 
-const createObj = () => {
-    console.log(localStorage)
+
+//Create obj with task properties
+const makeTask = (title, description, date, priority, id) => {
+    id = Date.now();
+    return {
+        title,
+        description,
+        date,
+        priority,
+        id
+    }
 }
+
+//Create obj with project properties
+const makeProject = (title, description) => {
+    let toDo = []
+    return {
+        title,
+        description,
+        toDo
+    }
+}
+
+//Create new task or project element and
+//push it to the corresponding local storage slot
+function addElement(el, title, description, date, priority, id) {
+    if (el == 'task'){
+    let newTask = makeTask(title, description, date, priority, id);
+    if (localStorage.getItem('tasks') == null) {
+        localStorage.setItem('tasks', '[]');
+    }
+    let oldTasks = localStorage.getObj('tasks');
+        oldTasks.push(newTask);
+            localStorage.setObj('tasks', oldTasks)
+}else if (el == 'project') {
+    let newProject = makeProject(title, description);
+    if (localStorage.getItem('projects') == null) {
+        localStorage.setItem('projects', '[]');
+    }
+    let oldProjects = localStorage.getObj('projects');
+        oldProjects.push(newProject);
+            localStorage.setObj('projects', oldProjects)
+    }
+}
+addElement('project', 'Have lunch', 'Eat soup with Sean')
