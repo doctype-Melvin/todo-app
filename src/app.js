6ae46f470916fec00a85d7e9ffe0645eaf2317cc
doctype-Melvin <doctype.melvin@gmail.com>
2022-06-22 11:49:11 +0200

    adds comments

--- src/app.js
+++ src/app.js
@@ -23,7 +23,7 @@ const makeTask = (title, description, date, priority, id) => {
 
 //Create obj with project properties
 const makeProject = (title, description) => {
-    let toDo = []
+    let toDo = [];
     return {
         title,
         description,
@@ -34,22 +34,31 @@ const makeProject = (title, description) => {
 //Create new task or project element and
 //push it to the corresponding local storage slot
 function addElement(el, title, description, date, priority, id) {
+    //New Task 
     if (el == 'task'){
-    let newTask = makeTask(title, description, date, priority, id);
-    if (localStorage.getItem('tasks') == null) {
-        localStorage.setItem('tasks', '[]');
+    let newTask = makeTask(title, description, date, priority, id); 
+    if (localStorage.getItem('tasks') == null) { //No tasks array in local storage?
+        localStorage.setItem('tasks', '[]'); //create tasks array in local storage
     }
-    let oldTasks = localStorage.getObj('tasks');
-        oldTasks.push(newTask);
-            localStorage.setObj('tasks', oldTasks)
+    let oldTasks = localStorage.getObj('tasks'); //Take old array
+        oldTasks.push(newTask); //and push new task
+            localStorage.setObj('tasks', oldTasks) //set the updated array in local storage
+    //New Project
 }else if (el == 'project') {
     let newProject = makeProject(title, description);
-    if (localStorage.getItem('projects') == null) {
-        localStorage.setItem('projects', '[]');
+    if (localStorage.getItem('projects') == null) { //No tasks array in local storage?
+        localStorage.setItem('projects', '[]'); //create projects array in local storage
     }
-    let oldProjects = localStorage.getObj('projects');
-        oldProjects.push(newProject);
-            localStorage.setObj('projects', oldProjects)
+    let oldProjects = localStorage.getObj('projects'); //Take old array
+        oldProjects.push(newProject); //and push new task
+            localStorage.setObj('projects', oldProjects) //set the updated array in local storage
     }
 }
-addElement('project', 'Have lunch', 'Eat soup with Sean')
+
+function addProjectTask(title, description, date, priority, id){
+    let oldTasks = JSON.parse(localStorage.getItem('projects'))[0].toDo;
+    let newTask = makeTask(title, description, date, priority, id);
+    oldTasks.push(newTask);
+}
+
+//addProjectTask('Bookstore', 'return book', 'today', 'high')
