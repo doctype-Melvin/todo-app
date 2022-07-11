a1fe9fa1797bbce163611e41400a986460704ae7
doctype-Melvin <doctype.melvin@gmail.com>
2022-07-11 12:14:25 +0200

    edits and removes tasks from tasks array

--- src/update.js
+++ src/update.js
@@ -10,7 +10,29 @@ import { newTask} from "./tasks";
 const taskData = (a, b, c) => {//Calls storage fn for the tasks array and creates new task obj
     addToStorage('tasks', (newTask(a, b, c)))
 };
+// taskData('Write code', 'this app', 'today');
+// taskData('Prep food', 'dinner', 'today');
+// taskData('Training', 'Upper body', 'today');
 
+//Edit task details
+const changeTask = (i, a, b, c) => {
+    let oldData = JSON.parse(localStorage.getItem('tasks'));
+    let task = oldData[i];
+    task.title = a;
+    task.note = b,
+    task.date = c;
+    oldData.splice(i, 1, task);
+    return localStorage.setItem('tasks', JSON.stringify(oldData))
+}
+// changeTask(0, 'Listen to music', 'classical', 'today')
+
+//Delete task
+const removeTask = (i) => {
+    let oldData = JSON.parse(localStorage.getItem('tasks'));
+    oldData.splice(i, 1);
+    return localStorage.setItem('tasks', JSON.stringify(oldData))
+}
+//removeTask(0)
 
 ///////       Projects
 //Add new data to storage location projects
